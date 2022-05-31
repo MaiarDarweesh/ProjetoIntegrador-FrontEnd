cd982567d844abe1454746397d0ee8561af1b6c1
miichaels <michael_araujo2012@hotmail.com>
2022-05-31 17:01:51 -0300

    Componente - Sobre nos criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule
