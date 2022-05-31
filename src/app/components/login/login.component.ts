e98711293d7f54e64bc3a7e4df12e4ef2f1fceee
Kakubia <kakubia@gmail.com>
2022-05-31 20:58:33 -0300

    Rotas funcionais

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+
 
   ngOnInit(): void {
   }
