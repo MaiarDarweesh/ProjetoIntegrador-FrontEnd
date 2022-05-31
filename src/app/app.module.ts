30b7c9436e13831423f4e0990bbc6693b514aa3c
MaiarDarweesh <maiar.darwish@gmail.com>
2022-05-31 14:36:30 -0300

    Criando o componente contato.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule
