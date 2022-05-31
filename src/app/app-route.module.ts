7585c3e00cb1750f3635897e585bb7cd645eeb22
Kakubia <kakubia@gmail.com>
2022-05-31 20:36:04 -0300

    Adicionado as rotas

--- src/app/app-route.module.ts
+++ src/app/app-route.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 const routes: Routes = [
 
-    {path:'' , redirectTo: 'login', pathMatch: 'full'},
+    // {path:'' , redirectTo: 'login', pathMatch: 'full'},
 
-    {path:'login', component: LoginComponent } 
+    {path:'login', component: LoginComponent },
+    {path:'cadastro', component: CadastroComponent },
+    {path:'home', component: HomeComponent },
+    {path:'sobre', component: SobreComponent },
+    {path:'contato', component: ContatoComponent }  
+        
   
 
 ];
