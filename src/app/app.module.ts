fa47b3219d78691ad5c129cb7d04bfcbdaf292f2
DoubleKang1 <rafaeldalves@outlook.com>
2022-06-06 10:05:29 -0300

    Atualização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-route.module';
 
 import { AppComponent } from './app.component';
-import { ContatoComponent } from './contato/contato.component';
-import { ProdutosComponent } from './produtos/produtos.component';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { ContatoComponent } from './components/contato/contato.component';
@@ -12,16 +10,16 @@ import { MenuComponent } from './components/menu/menu.component';
 import { SobreComponent } from './components/sobre/sobre.component';
 import { HomeComponent } from './components/home/home.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContatoComponent,
-    ProdutosComponent
+    ProdutosComponent,
     LoginComponent,
     MenuComponent,
     SobreComponent,
-    ContatoComponent,
     CadastroComponent,
     HomeComponent,
     RodapeComponent,
