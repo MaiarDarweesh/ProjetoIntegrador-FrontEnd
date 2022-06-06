6bc905a109834771a01edcf69525d17a0d40d046
ThamirisLisboa <102323316+ThamirisLisboa@users.noreply.github.com>
2022-06-06 10:40:30 -0300

    atualização de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'' , redirectTo: 'login', pathMatch: 'full'},
+
+  {path:'login', component: LoginComponent },
+  {path:'cadastro', component: CadastroComponent },
+  {path:'home', component: HomeComponent },
+  {path:'sobre', component: SobreComponent},
+  {path:'contato', component: ContatoComponent }  
+      
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-route.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
