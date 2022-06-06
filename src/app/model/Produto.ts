ac70297077164231b5d36d7666c1daf4c6207030
ThamirisLisboa <102323316+ThamirisLisboa@users.noreply.github.com>
2022-06-06 12:19:25 -0300

    criação da model de usuario e o service

---
+++ src/app/model/Produto.ts
@@ -0,0 +1,13 @@
+import { Usuario } from "./Usuario";
+
+export class Produto {
+
+public id: number;
+public nome: string;
+public descricao: string;
+public quantidade: number;
+public preco: number;
+public foto: string;
+public usuario: Usuario;
+public categoria: Categoria; 
+}
